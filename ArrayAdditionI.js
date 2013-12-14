c2c10bcb91c26b85a2e4bb679684a8de6a8c740d
TWHarr <tharrison06@gmail.com>
2013-12-14 10:37:38 -0800

    this works!
    based on
    https://github.com/leaena/coderbyte/blob/master/17arrayadditioni.js
    This had me stumped for a really long time, so I walked through her
    code line by line to make sure i fully understood what this code is
    doing. I have no idea why they would include this as an “easy”
    challenege; it was significantly harder than any of the others.

--- ArrayAdditionI.js
+++ ArrayAdditionI.js
@@ -3,18 +3,32 @@
 function compareNumbers(a,b) {
 	return a - b;
 }
+
 function ArrayAdditionI(arr) {
-	var result = false;
-	var sorted = arr.sort(compareNumbers);
-	var largest = sorted[sorted.length-1];
-	var addition = sorted[0];
-	for (i = 1; i < sorted.length; i ++) {
-		addition += sorted[i];
-		if (addition === largest) {
-			result = true;
+	arr.sort(compareNumbers); // sorting the numbers in numerical order
+	var largest = arr.pop(); // pop off the last (hence, largest) number of the array
+	var sum = 0;
+	for (var i = 0; i < arr.length; i ++) {
+		sum += arr[i]; //loop through the array, and add index number i to var sum
+		for (var j = 0; j < arr.length; j++) { //start a new loop, and
+			if (i != j) { // add all other numbers to sum (excluding i and largest)
+				sum += arr[j]; 
+				if (sum === largest){ // if sum matches largest, you can add up to largest
+					return true; 
+				}
+			}
 		}
+		for (var k = 0; k < arr.length; k ++) { // if you can't reach the largest starting with arr[0]
+			if (i != k) { 
+				sum -= arr[k]; // subtract numbers from the start
+				if (sum === largest) { // to see if you can reach the largest
+					return true;
+				}
+			}
+		}
+		sum = 0; // if you still can't match largest starting with arr[0], reset the loop and try with arr[1]
 	}
-	return result;
+	return false;
 }
    
 // keep this function call here 
