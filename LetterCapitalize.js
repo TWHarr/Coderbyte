4aa98cc4aa1aa15d3cafc67457327224d9adcee0
Tim Harrison <THarrison06@gmail.com>
2013-12-16 14:43:21 -0800

    2nd pass, comment/refactor

--- LetterCapitalize.js
+++ LetterCapitalize.js
@@ -1,13 +1,17 @@
+/* Using the JavaScript language, have the function LetterCapitalize(str) 
+take the str parameter being passed and capitalize the first letter of each 
+word. Words will be separated by only one space. */
+
 function LetterCapitalize(str) {
-	var newStr = str[0].toUpperCase();
-	for (i = 1; i < str.length; i ++){
-		if (str[i - 1] != " "){
-			newStr = newStr + str[i];
+	var newStr = str[0].toUpperCase(); // always capitalize the first letter of the sentence
+	for (i = 1; i < str.length; i ++){ // loop through the remaining characters of the sentence
+		if (str[i - 1] != " "){ // if the character preceding i is a space, it's a new word.
+			newStr += str[i]; //add it to the result, capitalized
 		} else{
-			newStr = newStr + str[i].toUpperCase();
+			newStr += str[i].toUpperCase(); // else just pass it to the new string.
 		}
 	}
-	return newStr; 
+	return newStr; //return the result
 }
    
 // keep this function call here 
