261035b8a4bbc33dcec9ed92e724f8b07cf08d99
Tim Harrison <THarrison06@gmail.com>
2013-12-16 10:44:36 -0800

    2nd pass, comment/refactor

--- LongestWord.js
+++ LongestWord.js
@@ -4,15 +4,14 @@ that are the same length, return the first word from the string with that
 length. Ignore punctuation and assume sen will not be empty. */
 
 function LongestWord(sen) {
-	nsen = sen.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
-	words = nsen.split(" ");
-	word = "a"
-	for (i = 0; i < words.length; i++){
-		if (words[i].length > word.length){
-			word = words[i]
+	words = sen.replace(/[^\w\s]/g,"").split(" "); //use regex to replace all punctuation with "" and split the string by word
+	word = ""; // initialize variable to store longest word
+	for (i = 0; i < words.length; i++){ //loop through the array holding the words
+		if (words[i].length > word.length){ //if the current word being examined is longer than the current longest word
+			word = words[i]; // the current word becomes the longest word
 		}
 	}
-	return word
+	return word; // return the longest word
 }
    
 // keep this function call here 
