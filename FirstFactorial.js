78e1050cd9331087edd74f21e9aa4fdf5a68c6e2
TWHarr <tharrison06@gmail.com>
2013-12-16 07:17:20 -0800

    2nd pass, comment

--- FirstFactorial.js
+++ FirstFactorial.js
@@ -1,12 +1,14 @@
-/* Have the function FirstFactorial(num) take the num parameter being passed and return the 
-factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)). For the test cases, the range 
+/* Have the function FirstFactorial(num) take the num 
+parameter being passed and return the 
+factorial of it (ie. if num = 4, return (4 * 3 * 2 * 1)). 
+For the test cases, the range 
 will be between 1 and 18. */
 
 function FirstFactorial(num) { 
-	for (i = num-1; i > 0; i--){
-		num *= i
+	for (i = num-1; i > 0; i--){ // start a for loop, starting from one less than num, down to 1
+		num *= i // multiply num by i. 
 	}
-	return num; 
+	return num; // return num, which has been factorialized.
 }
    
 // keep this function call here 
