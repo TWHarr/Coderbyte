5c4eb6f5691de260b9544c724b2c4a2018fb04ff
Tim Harrison <THarrison06@gmail.com>
2013-12-16 14:35:39 -0800

    2nd pass, comment

--- SimpleAdding.js
+++ SimpleAdding.js
@@ -3,11 +3,11 @@
 //parameter num will be any number from 1 to 1000. 
 
 function SimpleAdding(num){
-	var sum = 0
-	for (i = 1; i <= num; i++){
-		sum += i
+	var sum = 0 // initialize sum at 0
+	for (i = 1; i <= num; i++){ //loop from 1 to the given number
+		sum += i // add that number to sum
 	}
-	return sum
+	return sum // return the sum
 }
 // keep this function call here 
 // to see how to enter arguments in JavaScript scroll down
