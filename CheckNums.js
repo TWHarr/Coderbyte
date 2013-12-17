ffe4702f2a05c94c1623416944fe2e2b9637374c
Tim Harrison <THarrison06@gmail.com>
2013-12-17 12:30:27 -0800

    2nd pass, no big changes needed.

--- CheckNums.js
+++ CheckNums.js
@@ -6,7 +6,7 @@ values are equal to each other then return the string -1. */
 function CheckNums(num1,num2) {
 	if (num1 > num2){
 		return false;
-	} else if (num2 > num1){
+	} else if (num1 < num2){
 		return true;
 	} else if (num1 === num2){
 		return -1;
