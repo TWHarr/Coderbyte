2212f15ae53a78163fbf01e36f1abee8e7bb08cc
TWHarr <tharrison06@gmail.com>
2013-12-17 19:43:20 -0800

    2nd pass, comment refactor.

--- DivisionStringified.js
+++ DivisionStringified.js
@@ -1,21 +1,22 @@
-/* Using the JavaScript language, have the function DivisionStringified(num1,num2) take both parameters being passed, divide num1 by num2, and return the result as a string with properly formatted commas. If an answer is only 3 digits long, return the number with no commas (ie. 2 / 3 should output "1"). For example: if num1 is 123456789 and num2 is 10000 the output should be "12,345". */
+/* Using the JavaScript language, have the function 
+DivisionStringified(num1,num2) take both parameters 
+being passed, divide num1 by num2, and return the 
+result as a string with properly formatted commas. 
+If an answer is only 3 digits long, return the number 
+with no commas (ie. 2 / 3 should output "1"). For 
+example: if num1 is 123456789 and num2 is 10000 
+the output should be "12,345". */
 
 function DivisionStringified(num1,num2) {
-    var rounded = String(Math.round(num1 / num2));
-    var roundedArr = rounded.split("").reverse();
-    if (rounded.length < 4){
-        return String(rounded);
-    } else {
-        for (var i = 0; i < roundedArr.length; i++){
-            if (i % 4 === 0){
-                roundedArr.splice(i,0,",");
-            }
-        }
-        roundedArr.shift();
-        roundedArr = roundedArr.reverse().join("");
-        return String(roundedArr);
-    }
+    return (Math.round(num1/num2)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+//divide and round, then convert to string. Use regex to insert commas.
+//regex explanation:
+// find any non word boundary that isn't followed by a digit and is preceeded by 3 digits. global match.
+// so basically it works its way back. the first three digits not followed by another digit are the last three (the hundreds)
+// then, it insers a comma at that boundary. It the next 3 digits not followed by another digits are the thousands
+// which are now followed by a comma. etc. 
+
    
 // keep this function call here 
 // to see how to enter arguments in JavaScript scroll down
