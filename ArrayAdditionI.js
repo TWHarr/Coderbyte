0fcf99634f8b2afe61163e67c2f7c83d2de4324b
Tim Harrison <THarrison06@gmail.com>
2013-12-17 17:07:48 -0800

    2nd pass, minor comments

--- ArrayAdditionI.js
+++ ArrayAdditionI.js
@@ -1,4 +1,10 @@
-/* Using the JavaScript language, have the function ArrayAdditionI(arr) take the array of numbers stored in arr and return the string true if any combination of numbers in the array can be added up to equal the largest number in the array, otherwise return the string false. For example: if arr contains [4, 6, 23, 10, 1, 3] the output should return true because 4 + 6 + 10 + 3 = 23. The array will not be empty, will not contain all the same elements, and may contain negative numbers. */
+/* Using the JavaScript language, have the function ArrayAdditionI(arr) 
+take the array of numbers stored in arr and return the string true if any 
+combination of numbers in the array can be added up to equal the largest 
+number in the array, otherwise return the string false. For example: if arr 
+contains [4, 6, 23, 10, 1, 3] the output should return true because 4 + 6 + 
+10 + 3 = 23. The array will not be empty, will not contain all the same 
+elements, and may contain negative numbers. */
 
 function compareNumbers(a,b) {
 	return a - b;
@@ -14,7 +20,7 @@ function ArrayAdditionI(arr) {
 			if (i != j) { // add all other numbers to sum (excluding i and largest)
 				sum += arr[j]; 
 				if (sum === largest){ // if sum matches largest, you can add up to largest
-					return true; 
+					return true; //break the loop and return true
 				}
 			}
 		}
@@ -22,13 +28,13 @@ function ArrayAdditionI(arr) {
 			if (i != k) { 
 				sum -= arr[k]; // subtract numbers from the start
 				if (sum === largest) { // to see if you can reach the largest
-					return true;
+					return true; //break the loop and return true
 				}
 			}
 		}
 		sum = 0; // if you still can't match largest starting with arr[0], reset the loop and try with arr[1]
 	}
-	return false;
+	return false; 
 }
    
 // keep this function call here 
